a21f12a8661f41f97e0ae760e593ce946b89fc63
DESKTOP-DDOA898\z1rk3l <tim-emmert@web.de>
2016-12-30 13:44:32 +0100

    no message

---
+++ Server Fahrtenapp/src/def/Fahrt.java
@@ -0,0 +1,26 @@
+package def;
+
+
+public class Fahrt {
+	
+	private String zeit,ort,kommentar,fahrer;
+	
+	public void set(String zeit,String ort,String kommentar,String fahrer)
+	{
+		this.zeit=zeit;
+		this.ort=ort;
+		this.kommentar=kommentar;
+		this.fahrer=fahrer;
+	}
+	public Fahrt(String zeit,String ort,String kommentar,String fahrer)
+	{
+		this.zeit=zeit;
+		this.ort=ort;
+		this.kommentar=kommentar;
+		this.fahrer=fahrer;
+	}
+	public String getString()
+	{
+		return fahrer+"#"+ort+"#"+zeit+"#"+kommentar;
+	}
+}
---
+++ Server Fahrtenapp/src/def/Main.java
@@ -0,0 +1,22 @@
+package def;
+
+import java.io.IOException;
+
+public class Main {
+	private Server server;
+	
+	public static void main(String[]args)
+	{
+		new Main();
+	}
+	public Main()
+	{
+		new Fahrten();
+		try {
+			server=new Server();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
